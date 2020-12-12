a7b43ae7a046cf101a0750726d003278ffcdcfb7
Evgeny Generalov <Evgeniy.Generalov@coxautoinc.com>
2020-12-12 20:03:38 +0300

    feat: add dotenv-flow

--- services/bff-service/src/main.ts
+++ services/bff-service/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv-flow';
+
+dotenv.config();
+
+const { BFF_PORT: PORT } = process.env;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  console.debug(`bff-service app listening at http://localhost:${PORT}`);
+  await app.listen(PORT);
 }
+
 bootstrap();
